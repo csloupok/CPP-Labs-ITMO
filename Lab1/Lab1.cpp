b549692056f9bd72bf8aab60462dbee73e75b611
eldarkasymov <csloupok@gmail.com>
2022-03-02 13:39:36 +0300

    polyline added

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -4,13 +4,10 @@
 class Point {
 private:
     double x, y;
-
 public:
-    Point(double x = 0, double y = 0) : x{x}, y{y} {}
-
-    Point(const Point &point) = default;
-
-    Point &operator=(const Point &point) {
+    Point(double x = 0, double y = 0) : x{x}, y{y} {}       // конструктор
+    Point(const Point &point) : x{point.x}, y{point.y} {}   // копирование
+    Point &operator=(const Point &point) {                  // присваивание
         if (this == &point)
             return *this;
         x = point.x;
@@ -25,10 +22,17 @@ public:
     double get_y() const {
         return y;
     }
+
+//    Point &move(double x, double y) {
+//        this->x += x;
+//        this->y += y;
+//        return *this;
+//    }
+
 };
 
 class FigureMethods {
-public:
+protected:
     virtual double getSquare() const = 0;
 
     virtual double getPerimeter() const = 0;
@@ -36,7 +40,18 @@ public:
 
 using PointList = std::vector<Point>;
 
-class Polyline {
+class Polyline : public FigureMethods {
+private:
+    PointList points;
+public:
+    Polyline(const PointList &pointList) : points{pointList} {}     // конструктор
+    Polyline(const Polyline &polyline) : points{polyline.points} {} // копирование
+    Polyline &operator=(const Polyline &polyline) {                 // присваивание
+        if (this == &polyline)
+            return *this;
+        points = polyline.points;
+        return *this;
+    }
 };
 
 class Polygon {
